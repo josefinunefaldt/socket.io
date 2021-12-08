400ab34eabc8ab8c34b1c9231448d6e1b4fed28e
josefinunefaldt <josefin.unefaldt.syned21jon@tucsweden.se>
2021-12-08 11:56:23 +0000

    nu funkar typing bättre

--- publik/javascript/klientscript.js
+++ publik/javascript/klientscript.js
@@ -1,6 +1,8 @@
 let socket = io();
  
 window.onload = () => { 
+    //variabel för promt användarnan, while loop not anvandarna promt
+
     let output = document.getElementById("output");
     let typing = document.getElementById("meddelande");
     let meddelandet = document.getElementById("input")   
@@ -8,17 +10,19 @@ window.onload = () => {
     document.getElementById("form").addEventListener("submit", (evt) => {
         evt.preventDefault();
         let meddelande = document.getElementById("input").value;
-        let anvandare = document.getElementById("namnet").value; 
+        let anvandare = document.getElementById("namnet").value; //
+
         socket.emit("chat", meddelande,anvandare); 
         document.getElementById("input").value = ""; 
         document.getElementById("namnet").value = ""; 
         let tid = new Date().toISOString().substr(0, 16);   
         let html = `<p>  (${tid}) du skrev ${meddelande}</p>`;
-        output.innerHTML = html;
+        output.innerHTML = html; 
      
     });
 
     socket.on("chat", (data,nickname)  => {
+        typing.innerHTML ="";
         let tid = new Date().toISOString().substr(0, 16); 
         let html = `<p> (${tid}): ${nickname} skrev ${data}</p>`;
         output.innerHTML = html;
@@ -30,8 +34,7 @@ window.onload = () => {
         output.innerHTML = html;
     })
    
-    meddelandet.addEventListener("keypress", function() {
-        typing.innerHTML ="";
+    meddelandet.addEventListener("keypress", function() { 
         let smeknamn = document.getElementById("namnet");
         socket.emit("typing", smeknamn.value);
     });
