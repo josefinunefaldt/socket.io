a0c4a630fb328f442c63b6d9c94e6a773416aab6
josefinunefaldt <josefin.unefaldt.syned21jon@tucsweden.se>
2021-12-11 15:38:02 +0000

    sista småändraningar

--- publik/javascript/klientscript.js
+++ publik/javascript/klientscript.js
@@ -1,53 +1,53 @@
  
 window.onload = () => { 
-let dittSmeknamn = prompt("Hej och välkommen till chatten! Skriv ditt smeknamn för att ansluta");
+let dittSmeknamn = prompt("Hej och välkommen till chatten! Skriv ditt smeknamn för att ansluta");// sparar smeknamnet i prompt
     
-    while (!dittSmeknamn){ 
+    while (!dittSmeknamn){  // så länge användaren inte skriver in ett smeknamn
         dittSmeknamn = prompt("Du måste skriva ett smeknamn för att komma till chatten!");
      }
-     let socket = io();
-     socket.emit("namn", dittSmeknamn);
+     let socket = io(); // kallar socket konstruktorn
+     socket.emit("namn", dittSmeknamn);// skickar smeknamnet till servern
 
-    let utskrift = document.getElementById("output");
-    let typing = document.getElementById("anvandareSkriver");
-    let meddelandet = document.getElementById("input")   
+    let utskrift = document.getElementById("output"); // hämtar id för att skriva ut chatmeddelanden
+    let typing = document.getElementById("anvandareSkriver");//hämtar id för att skriva ut vem som som skriver
+    let meddelandet = document.getElementById("input"); // hämtar id för chatrutan 
 
-    document.getElementById("form").addEventListener("submit", (evt) => {
-        evt.preventDefault();
-        let meddelande = document.getElementById("input").value;
+    document.getElementById("form").addEventListener("submit", (evt) => { //händelselyssnare, när användaren klickar på "skicka-knappen"
+        evt.preventDefault();// hindrar sidan från att laddas om
+        let meddelande = document.getElementById("input").value; // hämtar värdet från användarens chatmeddelande
 
-        socket.emit("chat", meddelande,dittSmeknamn); 
-        document.getElementById("input").value = ""; 
-        let tid = new Date().toISOString().substr(0, 16);   
-        let html = ` (${tid}) du skrev ${meddelande}`;
-        utskrift.innerHTML = html; 
+        socket.emit("chat", meddelande,dittSmeknamn);// tar första steget för händelser i chatten. "skickar" meddelande och smeknamn
+        document.getElementById("input").value = ""; //återställer användarens chatmeddelande
+        let tid = new Date().toISOString().substr(0, 16);   //sparar datum samt timmar och minuter i en variabel  
+        let html = ` (${tid}) du skrev ${meddelande}`; // sparar tid och användarens inmatning i en textsträng
+        utskrift.innerHTML = html; //skriver ut textsträngen
      
     });
 
     socket.on("chat", (data,nickname)  => {
-        typing.innerHTML ="";
-        let tid = new Date().toISOString().substr(0, 16); 
-        let html = ` (${tid}): ${nickname} skrev ${data}`;
-        utskrift.innerHTML = html;
+        typing.innerHTML =""; //återställer typing när användaren har klickat på skickat
+        let tid = new Date().toISOString().substr(0, 16); //sparar datum samt timmar och minuter i en variabel 
+        let html = ` (${tid}): ${nickname} skrev ${data}`; //sparar datum, smeknamn och data i en textsträng
+        utskrift.innerHTML = html;// skriver ut textsträngen
     })
 
-    socket.on("announcement", (inmat) => { 
-        let html = ` ${inmat}`;
-        utskrift.innerHTML = html;
+    socket.on("besked", (inmat) => { //funktion som väntar på att någonting händer på servern. när någon disconnect startar den här funktionen
+        let html = ` ${inmat}`; // sparar den skickade textsträngen från index i en variabel
+        utskrift.innerHTML = html; // skriver ut textsträngen ovan, att en klient lämnade och hur många som är anslutna
     })
    
-    meddelandet.addEventListener("keypress", function() { 
-        socket.emit("typing", dittSmeknamn);
+    meddelandet.addEventListener("keypress", function() { // en funktion som startar när användaren börjar skriva i chatrutan
+        socket.emit("typing", dittSmeknamn); //skickar smeknamnet till servern
     })
  
-    socket.on("typing", function(data) {
-        typing.innerHTML ="💬" +" " + data + " skriver";
+    socket.on("typing", function(data) { 
+        typing.innerHTML ="💬" +" " + data + " skriver"; //skriver ut det sparade smeknamnet skriver
 
      }); 
      
-     socket.on("onlineAnvandare", (data) => {
-        let uppKoppling = document.getElementById("onlineLista");
-        uppKoppling.innerHTML=  data;
+     socket.on("onlineAnvandare", (data) => { //triggas när servern skickar anslutna
+        let uppKoppling = document.getElementById("onlineLista"); //hämtar id för att skriva ut inloggade
+        uppKoppling.innerHTML=  data; //skriver ut alla anslutna
    
     });
  
